be568c99137f97b3337ad5c27dfbe8d98540f2ce
mihailsinM <mihailsin49@gmail.com>
2023-05-28 22:29:56 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-
-
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
 
 const Slider = (props) => {
 
@@ -18,7 +20,7 @@ const Slider = (props) => {
             "https://images.unsplash.com/photo-1555001356-333dbc0f8b48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
             "https://plus.unsplash.com/premium_photo-1685030411267-c9bffe9b8c50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3N3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
         ]
-    }, []);
+    }, [slide]);
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
@@ -27,6 +29,18 @@ const Slider = (props) => {
     function toggleAutoplay() {
         setAutoplay(autoplay => !autoplay);
     }
+
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide])
+
+    useEffect(() => {
+        console.log('styles!');
+    }, [style])
 
     return (
         <Container>
@@ -37,6 +51,9 @@ const Slider = (props) => {
                 <div className="text-center mt-5">Active slide {slide} <br />
                     {autoplay ? 'Auto play.' : null}
                 </div>
+
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
+
                 <div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
